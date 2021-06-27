56cb7c95a88bcac6ede9f0a8726898c4ecc594ef
Christian Paul <info@jaller.de>
2021-06-27 21:09:40 +0200

    Globally catch errors

---
+++ src/error-catcher.js
@@ -0,0 +1,9 @@
+window.onerror = function (msg, url, line, col, error) {
+    // Note that col & error are new to the HTML 5 spec and may not be 
+    // supported in every browser.  It worked for me in Chrome.
+    let extra = !col ? '' : '\ncolumn: ' + col;
+    extra += !error ? '' : '\nerror: ' + error;
+
+    // You can view the information in an alert to see things working like this:
+    alert("Uncaught error.\nPlease report this and consider reloading the page.\n\n" + msg + "\nurl: " + url + "\nline: " + line + extra);
+};
