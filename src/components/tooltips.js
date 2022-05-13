b16368ef32384058aa17d62d3cb82a2a7adf7f1b
Christian Paul <info@jaller.de>
2022-05-13 09:43:58 +0200

    Add tooltip component

---
+++ src/components/tooltips.js
@@ -0,0 +1,24 @@
+import {
+    html,
+    useCallback,
+    useState,
+} from '../node_modules/htm/preact/standalone.module.js';
+
+export function MoreInfoTooltip({children}) {
+    const [open, setOpen] = useState(false);
+
+    const handleClick = useCallback(() => {
+        setOpen(value => !value);
+    }, []);
+
+    return html`
+        <span class="tooltip-container">
+            <button type="button" aria-label="more info" onClick=${handleClick}>i</button>
+            <span role="status">
+                ${open && html`
+                    <span class="toggletip-bubble">${children}</span>
+                `}
+            </span>
+        </span>
+    `;
+}
