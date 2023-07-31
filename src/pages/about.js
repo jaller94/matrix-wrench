8389f593b1de0325360e46100f0258977c386acf
Christian Paul <info@jaller.de>
2023-07-31 19:41:49 +0200

    Update About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,10 @@
 import { html } from '../node_modules/htm/preact/standalone.module.js';
 import { AppHeader } from '../components/header.js';
 
+function ExternalLink(props) {
+    return html`<a ...${props}/>`;
+}
+
 export default function AboutPage() {
     return html`
         <${AppHeader} backUrl="#">About</>
@@ -12,25 +16,26 @@ export default function AboutPage() {
             A common task is to view and edit a room state, e.g. to change the power levels. It works with access tokens of regular users and appservices (bridges and complex bots). If you give it an appservice token you can access any room the appservice has access to, allowing to easily debug and administrate bridges.
         </p>
         <p>
-            Furthermore, a few tasks around homeserver administration are supported, like listing media files in unencrypted rooms. The majority of features uses the <a href="https://spec.matrix.org/">standardized Matrix protocol</a>. If a feature makes use of the Synapse Admin API, this is noted.
+            Furthermore, a few tasks around homeserver administration are supported, like listing media files in unencrypted rooms. The majority of features use the standardized <${ExternalLink} href="https://spec.matrix.org/">Matrix protocol</a>. If a feature makes use of the Synapse Admin API, this is noted.
         </p>
+        <h2>Common tasks</h2>
         <h2>Identities</h2>
         <p>
             You can manage multiple logins to various homeservers. An identity is a combination of a homeserver URL and an access token. The identity name is only used for identification within Matrix Wrench.
         </p>
         <h2>Privacy and Security</h2>
         <p>
-            No information about your homeserver or actions is sent to the author or host of this web application. Identities are optionally stored in your browser. To eliminate the risk of a malicious release which could compromise your Matrix access tokens, download the application's source code and host it on a static HTTP(S) server.
+            No information about your homeserver or actions is sent to the developer or host of this web application. Identities are optionally stored in your browser's <${ExternalLink} href="https://developer.mozilla.org//docs/Web/API/Window/localStorage">localStorage</a>. To eliminate the risk of a malicious release which could compromise your Matrix access tokens, download the application's source code and host it on a static HTTP(S) server.
         </p>
         <p>
-            Matrix Wrench is my hobby project without a security audit or peer review. I use it at work and try to apply some best practices from my professional work to the development, however, Matrix Wrench is neither backed nor endorsed by my employer. There are a few unit tests that are automatically run on every commit. The project's dependencies are currently limited to <a href="https://www.npmjs.com/package/htm">htm</a> for rendering the interface.
+            Matrix Wrench is my hobby project without a security audit or peer review. I use it at work and try to apply some best practices from my professional work to the development, however, Matrix Wrench is neither backed nor endorsed by my employer. There are a few unit tests that are automatically run on every commit. The project's dependencies are currently limited to <${ExternalLink} href="https://www.npmjs.com/package/htm">htm</a> for rendering the interface.
         </p>
         <h2>Development</h2>
-        <p>Please file issues and request features on the <a href="https://gitlab.com/jaller94/matrix-wrench/-/issues">issue page on Gitlab.com</a>.</p>
+        <p>Found a bug? Want a feature? Please use the <${ExternalLink} href="https://gitlab.com/jaller94/matrix-wrench/-/issues">issue tracker on Gitlab.com</a>.</p>
         <ul>
-            <li>Code: <a href="https://gitlab.com/jaller94/matrix-wrench">Matrix Wrench on Gitlab.com</a></li>
-            <li>License: <a href="https://choosealicense.com/licenses/apache-2.0/">Apache 2.0</a></li>
-            <li>Author: <a href="https://chrpaul.de/about">Christian Paul</a></li>
+            <li>Code: <${ExternalLink} href="https://gitlab.com/jaller94/matrix-wrench">Matrix Wrench on Gitlab.com</a></li>
+            <li>License: <${ExternalLink} href="https://choosealicense.com/licenses/apache-2.0/">Apache 2.0</a></li>
+            <li>Author: <${ExternalLink} href="https://chrpaul.de/about">Christian Paul</a></li>
         </ul>
     `;
 }
