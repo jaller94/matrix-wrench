ab7a1791934af2dacfae5e1b0950b1196bbf3c2a
Christian Paul <info@jaller.de>
2024-06-13 20:04:00 +0800

    Finish dark mode v1

--- build.ts
+++ build.ts
@@ -3,10 +3,17 @@ import lightningcss from 'bun-lightningcss';
 
 await Bun.build({
   entrypoints: ['./src/index.js'],
-  minify: true,
+  minify: {
+    identifiers: false,
+    syntax: true,
+    whitespace: true,
+  },
+  target: 'browser',
   outdir: './public',
   plugins: [
     copy('static/', 'public'),
     lightningcss(),
   ],
+}).catch(err => {
+  console.error(err);
 });
